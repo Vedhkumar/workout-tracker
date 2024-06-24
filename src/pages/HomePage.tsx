4de17a08c62656292b7c8afcecb7409dd1476d96
Vedh Kumar <vedhkumar237@gmail.com>
2024-06-25 00:36:43 +0530

    project setup with all dependencies

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import { Avatar } from "@radix-ui/react-avatar";
+import { Badge } from "lucide-react";
+
+// import { Button } from "@/components/ui/button";
+function HomePage() {
+  return (
+    <div>
+      Home Page
+      {/* <Button /> */}
+      <Badge />
+      <Avatar />
+    </div>
+  );
+}
+
+export default HomePage;
